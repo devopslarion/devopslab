49ce32b277474414e9f8dd9c9c66dd1a09bed01f
vunv <vunv@pc-162.local.elarion.com>
2018-10-02 16:06:00 +0700

    ok

--- scripts/create_volume.sh
+++ scripts/create_volume.sh
@@ -1,7 +1,8 @@
+dockeraws='sudo docker -H localhost:2374'
 dockeraws volume create \
 -d "cloudstor:aws" \
 --opt ebstype=io1 \
 --opt size=4 \
 --opt iops=100 \
 --opt backing=relocatable \
-portainer.data
+$1
--- scripts/git_clone_resource.sh
+++ scripts/git_clone_resource.sh
@@ -10,3 +10,5 @@ dotenv
 
 #ssh -i ~/.ssh/vunv docker@34.229.247.64 git clone https://github.com/devopslarion/devopslab.git
 ssh docker@${AWS_MASTER_NODE} git clone https://github.com/devopslarion/devopslab.git
+ssh -A docker@${AWS_MASTER_NODE} "ssh ${AWS_WORKER_NODE_1} git clone https://github.com/devopslarion/devopslab.git"
+ssh -A docker@${AWS_MASTER_NODE} "ssh ${AWS_WORKER_NODE_2} git clone https://github.com/devopslarion/devopslab.git"
